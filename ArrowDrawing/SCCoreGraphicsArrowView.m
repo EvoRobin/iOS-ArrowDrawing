59243104729f32663fc6f4ed056e9f25b4d03cd0
Sam Davies <sdavies@shinobicontrols.com>
2014-03-07 15:57:29 +0000

    Adds an arrow head :)

--- ArrowDrawing/SCCoreGraphicsArrowView.m
+++ ArrowDrawing/SCCoreGraphicsArrowView.m
@@ -29,6 +29,7 @@
         // Set some defaults
         self.color = [UIColor redColor];
         self.lineThickness = 2.0;
+        self.headSize = 20;
     }
     return self;
 }
@@ -50,20 +51,27 @@
     
     // Now draw the end
     CGPoint endVector = [self determinePointOnQuadBezierAtPosition:0.95 startPoint:start endPoint:end controlPoint:control];
-    endVector.x = end.x - endVector.x;
-    endVector.y = end.y - endVector.y;
+    endVector.x = endVector.x - end.x;
+    endVector.y = endVector.y - end.y;
     
     // Out at right angles
+    CGPoint perpVector = [self perpendicularToVector:endVector length:self.headSize / 2];
+
+    // Back from tip
+    CGPoint footOfArrow = [self addVector:end toVector:[self resizeVector:endVector length:self.headSize]];
+    CGPoint arrowSide1 = [self addVector:footOfArrow toVector:perpVector];
+    CGPoint arrowSide2 = [self addVector:footOfArrow toVector:[self multiplyVector:perpVector byScalar:-1]];
     
     // Draw line to point
+    CGContextMoveToPoint(cxt, arrowSide1.x, arrowSide1.y);
+    CGContextAddLineToPoint(cxt, end.x, end.y);
     
     // Then to other out
-    
-    // Do we close it?
+    CGContextAddLineToPoint(cxt, arrowSide2.x, arrowSide2.y);
     
     // Stroke it
-    
-    // Do we fill it?
+    CGContextStrokePath(cxt);
+
     
 }
 
@@ -81,6 +89,38 @@
     CGFloat x = (1-t) * ((1-t) * start.x + t * control.x) + t * ((1-t) * control.x + t * end.x);
     CGFloat y = (1-t) * ((1-t) * start.y + t * control.y) + t * ((1-t) * control.y + t * end.y);
     return CGPointMake(x, y);
+}
+
+- (CGPoint)perpendicularToVector:(CGPoint)vector length:(CGFloat)length
+{
+    CGPoint perpVector;
+    perpVector.x =   vector.y;
+    perpVector.y = - vector.x;
+    return [self resizeVector:perpVector length:length];
+}
+
+- (CGPoint)resizeVector:(CGPoint)vector length:(CGFloat)length
+{
+    CGFloat currentLength = [self vectorLength:vector];
+    return [self multiplyVector:vector byScalar:(length / currentLength)];
+}
+
+- (CGFloat)vectorLength:(CGPoint)vector
+{
+    return sqrt(vector.x * vector.x + vector.y * vector.y);
+}
+
+- (CGPoint)addVector:(CGPoint)v1 toVector:(CGPoint)v2
+{
+    CGPoint sum;
+    sum.x = v1.x + v2.x;
+    sum.y = v1.y + v2.y;
+    return sum;
+}
+
+-(CGPoint)multiplyVector:(CGPoint)vector byScalar:(CGFloat)scalar
+{
+    return CGPointMake(vector.x * scalar, vector.y * scalar);
 }
 
 
